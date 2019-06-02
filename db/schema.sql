9949c59f6b0753a8a2e01cc0ac138673cadabdaf
Isin <isinselen@gmail.com>
2019-06-01 18:47:34 -0700

    working on heroku app

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS burgers_db;
+USE burgers_db;
+
+-- If the table already exists, remove it before trying to create the table again
+DROP TABLE IF EXISTS burgers;
+
+-- Create the burgers table
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOL DEFAULT false,
+    PRIMARY KEY (id)
+);
+
+USE burger_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Umami Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegie Burger', FALSE);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Umami Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegie Burger', FALSE);
